813952bbf161c2cdb0f258bfa4be452a5c1c5d48
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-19 22:07:52 +0530

    auth responce updated

---
+++ DTOs/Authentication/AuthResponse.cs
@@ -0,0 +1,14 @@
+namespace SmartHealthcare.API.DTOs.Authentication;
+
+public class AuthResponse
+{
+    public string Token { get; set; } = string.Empty;
+
+    public Guid UserId { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Role { get; set; } = string.Empty;
+}
